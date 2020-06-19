bb49bb1d2d7e7bb78335c6a7e362cb6adec1d674
Kim <st104889@stud.uni-stuttgart.de>
2020-06-19 16:01:07 +0200

    update runExperiment.sh script

--- binCreatedFromCMD/runExperiment.sh
+++ binCreatedFromCMD/runExperiment.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 repCount=3
-server=80Core
+
+#server=24Cores
+server=80Cores
+#server=96Cores
+
 perfArguments=L1-dcache-store,L1-dcache-store-misses,LLC-store,LLC-store-misses
-sudo perf stat -e $perfArguments -dd java -jar repetitions$repCount/1ThreadsMatrixExp.jar &>>./expResults/$server/output1.txt
+
+sudo perf stat -e $perfArguments -dd java -jar repetitions$repCount/1ThreadsMatrixExp.jar &>>./expResults/$server/repetitions$repCount/output1.txt
 echo "Success1"
 
-sudo perf stat -e $perfArguments -dd java -jar repetitions$repCount/2ThreadsMatrixExp.jar &>>./expResults/$server/output2.txt
+sudo perf stat -e $perfArguments -dd java -jar repetitions$repCount/2ThreadsMatrixExp.jar &>>./expResults/$server/repetitions$repCount/output2.txt
 echo "Success2"
 
-sudo perf stat -e $perfArguments -dd java -jar repetitions$repCount/4ThreadsMatrixExp.jar &>>./expResults/$server/output4.txt
+sudo perf stat -e $perfArguments -dd java -jar repetitions$repCount/4ThreadsMatrixExp.jar &>>./expResults/$server/repetitions$repCount/output4.txt
 echo "Success4"
 
-sudo perf stat -e $perfArguments -dd java -jar repetitions$repCount/8ThreadsMatrixExp.jar &>>./expResults/$server/output8.txt
+sudo perf stat -e $perfArguments -dd java -jar repetitions$repCount/8ThreadsMatrixExp.jar &>>./expResults/$server/repetitions$repCount/output8.txt
 echo "Success8"
 
-sudo perf stat -e $perfArguments -dd java -jar repetitions$repCount/16ThreadsMatrixExp.jar &>>./expResults/$server/output16.txt
+sudo perf stat -e $perfArguments -dd java -jar repetitions$repCount/16ThreadsMatrixExp.jar &>>./expResults/$server/repetitions$repCount/output16.txt
 echo "Success16"
 
-sudo perf stat -e $perfArguments -dd java -jar repetitions$repCount/32ThreadsMatrixExp.jar &>>./expResults/$server/output32.txt
+sudo perf stat -e $perfArguments -dd java -jar repetitions$repCount/32ThreadsMatrixExp.jar &>>./expResults/$server/repetitions$repCount/output32.txt
 echo "Success32"
