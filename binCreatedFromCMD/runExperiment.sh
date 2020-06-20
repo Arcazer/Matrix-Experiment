8fc5a40d3f9af0a2be7c7186b1f2b4360802ff7b
Kim <st104889@stud.uni-stuttgart.de>
2020-06-20 20:53:34 +0200

    update runExperiment.sh with 64Threads execution

--- binCreatedFromCMD/runExperiment.sh
+++ binCreatedFromCMD/runExperiment.sh
@@ -7,7 +7,7 @@ server=80Cores
 
 
 
-for i in 1 2 4 8 16 32
+for i in 1 2 4 8 16 20 24 32 48 64
 do
 
   perfArguments="stat -x; -ddd -o expResults/$server/repetitions$repCount/perfOutput${i}.csv -e L1-dcache-store,L1-dcache-store-misses,LLC-store,LLC-store-misses"
@@ -18,5 +18,3 @@ do
 done
 
 mv *.csv expResults/$server/repetitions$repCount/
-
-
