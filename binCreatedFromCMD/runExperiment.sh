cbe6e18b1143c7ebee155d3ecce0f0719832042c
Kim <st104889@stud.uni-stuttgart.de>
2020-07-06 18:05:52 +0200

    add l3 stall perf event

--- binCreatedFromCMD/runExperiment.sh
+++ binCreatedFromCMD/runExperiment.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-repCount=100
+repCount=1
 
 #server=24Cores
-server=80Cores
-#server=96Cores
+#server=80Cores
+server=96Cores
 
 
 
 for i in 1 2 4 6 8 10 12 14 16 18 20 24 32 40 44 48 56 64
 do
 
-  perfArguments="stat -x; -ddd -o expResults/$server/repetitions$repCount/perfOutput${i}.csv -e L1-dcache-store,L1-dcache-store-misses,LLC-store,LLC-store-misses"
+  perfArguments="stat -x; -ddd -o expResults/$server/repetitions$repCount/perfOutput${i}.csv -e L1-dcache-store,L1-dcache-store-misses,LLC-store,LLC-store-misses,cycle_activity.stalls_l2_miss,cycle_activity.stalls_l3_miss,longest_lat_cache.reference,longest_lat_cache.miss"
 
   sudo perf $perfArguments java -jar repetitions$repCount/${i}ThreadsMatrixExp.jar &>>./expResults/$server/repetitions$repCount/consoleOutput$i.txt
   echo "Success$i"
