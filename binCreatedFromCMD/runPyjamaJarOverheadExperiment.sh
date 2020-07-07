7f307a74c1351f059eaf41bd1b6cc994a699149b
Kim <st104889@stud.uni-stuttgart.de>
2020-07-07 17:59:26 +0200

    add pyjamajar overhead experiment

---
+++ binCreatedFromCMD/runPyjamaJarOverheadExperiment.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+java -jar repetitions1/1ThreadsMatrixExp.jar
+
+# java -jar singleThreadSequentialRun250Rep.jar
+
+# java -jar repetitions250/1ThreadsMatrixExp.jar
+
+# java -jar singleThreadSequentialRun250Rep.jar
+
+mkdir expResults/pyjamaOverhead/
+mv *.csv expResults/pyjamaOverhead/
+
